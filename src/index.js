651344958785c108cb13af759f86b55f6eace402
Андрей Сельдюков <a.seldukov@onpoint.ru>
2021-06-12 23:57:55 +0300

    getPermission

--- src/index.js
+++ src/index.js
@@ -137,7 +137,7 @@ if (!isTouchDevice) {
 
   const clearRect = () => {
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  }
+  };
 
   let x;
   document.addEventListener(
@@ -148,6 +148,32 @@ if (!isTouchDevice) {
     },
     false
   );
+} else {
+  if (location.protocol != "https:") {
+    location.href =
+      "https:" +
+      window.location.href.substring(window.location.protocol.length);
+  }
+  function permission() {
+    if (
+      typeof DeviceMotionEvent !== "undefined" &&
+      typeof DeviceMotionEvent.requestPermission === "function"
+    ) {
+      // (optional) Do something before API request prompt.
+      DeviceMotionEvent.requestPermission()
+        .then((response) => {
+          // (optional) Do something after API prompt dismissed.
+          if (response == "granted") {
+            window.addEventListener("devicemotion", (e) => {
+              // do something for 'e' here.
+            });
+          }
+        })
+        .catch(console.error);
+    } else {
+      alert("DeviceMotionEvent is not defined");
+    }
+  }
+  permission()
+  window.addEventListener("deviceorientation", (e) => console.log(e), true);
 }
-
-else window.addEventListener("deviceorientation", (e) => console.log(e), true);
