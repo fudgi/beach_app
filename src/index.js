9426f91c6451cab6314c024633e6ccd30b709e25
Андрей Сельдюков <a.seldukov@onpoint.ru>
2021-06-13 00:00:56 +0300

    anim

--- src/index.js
+++ src/index.js
@@ -149,31 +149,10 @@ if (!isTouchDevice) {
     false
   );
 } else {
-  if (location.protocol != "https:") {
-    location.href =
-      "https:" +
-      window.location.href.substring(window.location.protocol.length);
-  }
-  function permission() {
-    if (
-      typeof DeviceMotionEvent !== "undefined" &&
-      typeof DeviceMotionEvent.requestPermission === "function"
-    ) {
-      // (optional) Do something before API request prompt.
-      DeviceMotionEvent.requestPermission()
-        .then((response) => {
-          // (optional) Do something after API prompt dismissed.
-          if (response == "granted") {
-            window.addEventListener("devicemotion", (e) => {
-              // do something for 'e' here.
-            });
-          }
-        })
-        .catch(console.error);
-    } else {
-      alert("DeviceMotionEvent is not defined");
-    }
-  }
-  permission()
-  window.addEventListener("deviceorientation", (e) => console.log(e), true);
+  const anim = () => {
+    setTimeout(() => {
+      requestAnimationFrame(generateLine);
+    }, 20);
+  };
+  requestAnimationFrame(anim);
 }
